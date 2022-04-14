bde5a7b4bec3ee5caa2348ed3dcd074c9a2b698b
hdvincelette <hdvincelette@gmail.com>
2022-04-14 12:51:48 -0800

    Update build.table.R

--- R/build.table.R
+++ R/build.table.R
@@ -1,7 +1,7 @@
 #' Build Tabular Data Dictionaries
 #'
-#' Transforms an mdEditor mdJSON data dictionary (imported as an R list) into a data frame.
-#' @param x An R list converted from an mdJSON data dictionary file.
+#' Translates a list object converted from an mdEditor mdJSON data dictionary file into a data frame.
+#' @param x A list object converted from an mdJSON data dictionary file.
 #' @param dictionary_num Default=1. An integer indicating the dictionary if there is more than one in the mdJSON file (i.e. if multiple dictionaries are exported together in mdEditor).
 #' @param entity_num Default=1. An integer indicating the entity if there is more than one in the mdJSON file.
 #' @return Returns a data frame corresponding to the mdJSON data dictionary.
@@ -13,8 +13,8 @@
 #' path<-system.file("extdata", "e.g.dictionary2.json", package = "mdJSONdictio")
 #' input.json <- rjson::fromJSON(file = path)
 #'
-#' # Transform R list to a data frame
-#' new.table<- build.table(x = input.json, dictionary_num = 1, entity_num = 1)
+#' # Translate list to data frame
+#' new.table<- mdJSONdictio::build.table(x = input.json, dictionary_num = 1, entity_num = 1)
 #'
 #' # Export table to disk
 #' write.csv(x = new.table, file = "e.g.dictionary2.csv", na="", row.names = FALSE)
