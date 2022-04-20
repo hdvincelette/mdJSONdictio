e8bae25c984c5da105fbde23767de55209da68d7
Hannah Vincelette <41804055+hdvincelette@users.noreply.github.com>
2022-04-19 22:02:03 -0800

    Update build.table.R

--- R/build.table.R
+++ R/build.table.R
@@ -142,8 +142,8 @@ build.table <- function(x, entity_num, dictionary_num) {
         blanktable$entityNum[b] <- b
       }
     }
-    blanktable$name <- "dataElement"
-    blanktable$value <- "dataElement"
+    blanktable$name <- "dataField"
+    blanktable$value <- "dataField"
 
     if (is.null(domainlist) == FALSE) {
       for (d in 1:length(domainlist)) {
