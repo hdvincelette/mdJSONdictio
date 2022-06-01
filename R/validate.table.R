57b4591f2e85fb48fa2704e082e8f0d51da4c094
Hannah Vincelette <41804055+hdvincelette@users.noreply.github.com>
2022-05-31 22:24:20 -0800

    Update validate.table.R

--- R/validate.table.R
+++ R/validate.table.R
@@ -20,7 +20,7 @@
 #' all.warnings<- validate.table(input.dict, input.data)
 #'
 #' # Export table to disk
-#' write(x = all.warnings, file = "e.g.validate.table.json")
+#' write.csv(x = all.warnings, file = "e.g.warnings.json")
 
 
 
@@ -47,12 +47,12 @@ validate.table <- function(x, y) {
     dplyr::filter(domainItem_value != "dataField") %>%
     dplyr::select(codeName, domainItem_value) %>%
     dplyr::group_by(codeName) %>%
-    dplyr::group_map(~ .x)
+    dplyr::group_map( ~ .x)
 
   names(dict.domain) <- input.dict %>%
     dplyr::filter(domainItem_value != "dataField") %>%
     dplyr::group_by(codeName) %>%
-    dplyr::group_map(~ .y) %>%
+    dplyr::group_map( ~ .y) %>%
     unlist()
 
   dict.datafield <- input.dict %>%
@@ -151,15 +151,16 @@ validate.table <- function(x, y) {
 
   data.NA <- input.data
 
-  for (a in 1:ncol(data.NA)) {
-    for (bb in 1:nrow(dict.miss)) {
-      if (colnames(data.NA[a]) == dict.miss$codeName[bb] &
-          dict.miss$missingValue[bb] %in% unique(data.NA[, a])) {
-        data.NA[a] <- utils::type.convert(data.NA[a], as.is = TRUE)
+  if (nrow(dict.miss) != 0) {
+    for (a in 1:ncol(data.NA)) {
+      for (bb in 1:nrow(dict.miss)) {
+        if (colnames(data.NA[a]) == dict.miss$codeName[bb] &
+            dict.miss$missingValue[bb] %in% unique(data.NA[, a])) {
+          data.NA[a] <- utils::type.convert(data.NA[a], as.is = TRUE)
+        }
       }
     }
   }
-
 
 
 
@@ -481,131 +482,128 @@ validate.table <- function(x, y) {
   ##############################################################################
 
   # Check unitsResolution
-  dict.res <- input.dict %>%
-    dplyr::filter(!is.na(unitsResolution))
 
-  data.res <- input.data %>%
-    dplyr::select(any_of(dict.res$codeName))
+  # dict.res <- input.dict %>%
+  #   dplyr::filter(!is.na(unitsResolution))
+  #
+  # data.res <- input.data %>%
+  #   dplyr::select(any_of(dict.res$codeName))
 
 
   ### Remove trailing zeros
   #purrr::map_df(stringr::str_remove, pattern = "^0+")
 
 
-  data.ndecimal <- NULL
-  dict.ndecimal <- NULL
 
-
-  for (a in 1:ncol(data.res)) {
-    # print(colnames(data.res[a]))
+  for (a in 1:ncol(input.data)) {
+    # print(colnames(input.data[a]))
     data.ndecimal <- NULL
+    dict.ndecimal <- NULL
 
-    for (bb in 1:nrow(dict.res)) {
-      if (colnames(data.res[a]) == dict.res$codeName[bb]) {
+    for (bb in 1:nrow(input.dict)) {
+      if (colnames(input.data[a]) == input.dict$codeName[bb] &
+          is.na(input.dict$unitsResolution[bb]) == FALSE) {
         data.ndecimal <-
-          nchar(gsub(".*\\.|^[^.]+$", "", as.character(data.res[, a])))
+          nchar(gsub(".*\\.|^[^.]+$", "", as.character(input.data[, a])))
 
         dict.ndecimal <-
-          nchar(gsub(".*\\.",
-                     "",
-                     as.character(dict.res$unitsResolution[bb])))
+          nchar(gsub(
+            ".*\\.",
+            "",
+            as.character(input.dict$unitsResolution[bb])
+          ))
+
+        # print(paste0(min(data.ndecimal, na.rm = TRUE), " (min)"))
+        # print(paste0(max(data.ndecimal, na.rm = TRUE), " (max)"))
+        # print(paste0(dict.ndecimal, " (dictionary)"))
+
+        if (is.null(dict.ndecimal) == FALSE) {
+          min.data.ndecimal <- min(data.ndecimal, na.rm = TRUE)
+          max.data.ndecimal <- max(data.ndecimal, na.rm = TRUE)
+
+          if (is.null(min.data.ndecimal) == FALSE &
+              min.data.ndecimal < dict.ndecimal) {
+            warnings.df <- warnings.df %>%
+              dplyr::add_row(
+                Num = nrow(warnings.df) + 1,
+                Variable = colnames(input.data[a]),
+                Category = "unitsResolution",
+                Message =  paste0(
+                  'Dataset variable contains entry value(s) with lower resolution than "unitsResolution" in dictionary'
+                )
+              )
+
+
+            if (is.null(max.data.ndecimal) == FALSE &
+                max.data.ndecimal  > dict.ndecimal) {
+              warnings.df <- warnings.df %>%
+                dplyr::add_row(
+                  Num = nrow(warnings.df) + 1,
+                  Variable = colnames(input.data[a]),
+                  Category = "unitsResolution",
+                  Message =  paste0(
+                    'Dataset variable contains entry value(s) with higher resolution than "unitsResolution" in dictionary'
+                  )
+                )
+            }
+
+          }
+        }
       }
     }
-
-    # print(paste0(min(data.ndecimal, na.rm = TRUE), " (min)"))
-    # print(paste0(max(data.ndecimal, na.rm = TRUE), " (max)"))
-    # print(paste0(dict.ndecimal, " (dictionary)"))
-
-    if (is.null(data.ndecimal) == FALSE &
-        min(data.ndecimal, na.rm = TRUE) < dict.ndecimal) {
-      warnings.df <- warnings.df %>%
-        dplyr::add_row(
-          Num = nrow(warnings.df) + 1,
-          Variable = colnames(input.data[a]),
-          Category = "unitsResolution",
-          Message =  paste0(
-            'Dataset variable contains entry value(s) with lower resolution than "unitsResolution" in dictionary'
-          )
-        )
-    }
-
-    if (is.null(data.ndecimal) == FALSE &
-        max(data.ndecimal, na.rm = TRUE) > dict.ndecimal) {
-      warnings.df <- warnings.df %>%
-        dplyr::add_row(
-          Num = nrow(warnings.df) + 1,
-          Variable = colnames(input.data[a]),
-          Category = "unitsResolution",
-          Message =  paste0(
-            'Dataset variable contains entry value(s) with higher resolution than "unitsResolution" in dictionary'
-          )
-        )
-    }
-
   }
-
 
 
   # Check fieldWidth
-  dict.width <- input.dict %>%
-    dplyr::filter(!is.na(fieldWidth))
 
-  data.width <- input.data %>%
-    dplyr::select(any_of(dict.width$codeName))
-
-
-  data.nchar <- NULL
-  dict.nchar <- NULL
-
-
-
-  for (a in 1:ncol(data.width)) {
-    # print(colnames(data.width[a]))
+  for (a in 1:ncol(input.data)) {
+    # print(colnames(input.data[a]))
     data.nchar <- NULL
+    dict.nchar <- NULL
 
-    for (bb in 1:nrow(dict.width)) {
-      for (aa in 1:nrow(data.width)) {
-        if (colnames(data.width[a]) == dict.width$codeName[bb]) {
-          data.nchar <- c(data.nchar, nchar(as.character(data.width[aa, a])))
+    for (bb in 1:nrow(input.dict)) {
+      if (colnames(input.data[a]) == input.dict$codeName[bb] &
+          is.na(input.dict$fieldWidth[bb]) == FALSE) {
+        data.nchar <-  nchar(as.character(input.data[, a]))
+
+        dict.nchar <- input.dict$fieldWidth[bb]
+
+        if (is.null(data.nchar) == FALSE) {
+          # print(paste0(max(data.nchar, na.rm = TRUE), " (max)"))
+          # print(paste0((dict.nchar), " (dictionary)"))
+
+
+          if (is.null(data.nchar) == FALSE &
+              max(data.nchar, na.rm = TRUE) > dict.nchar) {
+            warnings.df <- warnings.df %>%
+              dplyr::add_row(
+                Num = nrow(warnings.df) + 1,
+                Variable = colnames(input.data[a]),
+                Category = "fieldWidth",
+                Message =  paste0(
+                  'Dataset variable contains entry value(s) that exceeds "fieldWidth" in dictionary'
+                )
+              )
+          }
+
         }
-      }
-      if (dict.width$codeName[bb] == colnames(data.width[a])) {
-        dict.nchar <- dict.width$fieldWidth[bb]
+
       }
     }
-
-    # print(paste0(max(data.nchar, na.rm = TRUE)," (max)"))
-    # print(paste0((dict.nchar)," (dictionary)"))
-
-
-    if (is.null(data.nchar) == FALSE &
-        max(data.nchar, na.rm = TRUE) > dict.nchar) {
-      warnings.df <- warnings.df %>%
-        dplyr::add_row(
-          Num = nrow(warnings.df) + 1,
-          Variable = colnames(input.data[a]),
-          Category = "fieldWidth",
-          Message =  paste0(
-            'Dataset variable contains entry value(s) that exceeds "fieldWidth" in dictionary'
-          )
-        )
-    }
-
   }
 
 
-
   # Check missingValue
-  dict.miss <- dict.datafield %>%
-    dplyr::filter(!is.na(missingValue))
 
-  data.miss <- input.data %>%
-    dplyr::select(any_of(dict.miss$codeName))
+  for (a in 1:ncol(input.data)) {
+    for (bb in 1:nrow(input.dict))
+      if (colnames(input.data[a]) == input.dict$codeName[bb] &
+          is.na(input.dict$missingValue[bb]) == FALSE &
+          NA %in% (unique(input.data[, a]))) {
 
-  for (a in 1:ncol(data.miss)) {
-    for (bb in 1:nrow(dict.miss))
-      if (colnames(data.miss[a]) == dict.miss$codeName[bb] &
-          NA %in% (unique(data.miss[, a]))) {
+        # print(paste0(colnames(input.data[a]) ))
+        # print(paste0(input.dict$missingValue[bb]))
+
         warnings.df <- warnings.df %>%
           dplyr::add_row(
             Num = nrow(warnings.df) + 1,
@@ -613,7 +611,7 @@ validate.table <- function(x, y) {
             Category = "missingValue",
             Message =  paste0(
               'Dataset variable contains blank values rather than "missingValue" in dictionary: ',
-              dict.miss$missingValue[bb]
+              input.dict$missingValue[bb]
             )
           )
       }
@@ -621,55 +619,54 @@ validate.table <- function(x, y) {
 
 
   # Check minValue
-  dict.min <- input.dict %>%
-    dplyr::filter(!is.na(minValue))
 
-  data.min <- input.data %>%
-    dplyr::select(any_of(dict.min$codeName))
+  for (a in 1:ncol(input.data)) {
+    for (bb in 1:nrow(input.dict))
+      if (colnames(input.data[a]) == input.dict$codeName[bb] &
+          is.na(input.dict$minValue[bb]) == FALSE) {
+        if (!NA %in% unique(input.data[, a]) |
+            NA %in% unique(input.data[, a]) &
+            length(unique(input.data[, a])) > 1) {
+          if (min(unique(input.data[, a]), na.rm = TRUE) < input.dict$minValue[bb]) {
+            warnings.df <- warnings.df %>%
+              dplyr::add_row(
+                Num = nrow(warnings.df) + 1,
+                Variable = colnames(input.data[a]),
+                Category = "minValue",
+                Message =  paste0(
+                  'Dataset variable contains entry value(s) less than "minValue" in dictionary'
+                )
+              )
+          }
 
-
-  for (a in 1:ncol(data.min)) {
-    for (bb in 1:nrow(dict.min))
-      if (colnames(data.min[a]) == dict.min$codeName[bb] &
-          min(unique(data.min[a]), na.rm = TRUE) < dict.min$minValue[bb]) {
-        warnings.df <- warnings.df %>%
-          dplyr::add_row(
-            Num = nrow(warnings.df) + 1,
-            Variable = colnames(input.data[a]),
-            Category = "minValue",
-            Message =  paste0(
-              'Dataset variable contains entry value(s) less than "minValue" in dictionary'
-            )
-          )
+        }
       }
-
   }
 
 
-
   # Check maxValue
-  dict.max <- input.dict %>%
-    dplyr::filter(!is.na(minValue))
 
-  data.max <- input.data %>%
-    dplyr::select(any_of(dict.max$codeName))
+  for (a in 1:ncol(input.data)) {
+    for (bb in 1:nrow(input.dict))
+      if (colnames(input.data[a]) == input.dict$codeName[bb] &
+          is.na(input.dict$maxValue[bb]) == FALSE) {
+        if (!NA %in% unique(input.data[, a]) |
+            NA %in% unique(input.data[, a]) &
+            length(unique(input.data[, a])) > 1) {
+          if (max(unique(input.data[, a]), na.rm = TRUE) > input.dict$maxValue[bb]) {
+            warnings.df <- warnings.df %>%
+              dplyr::add_row(
+                Num = nrow(warnings.df) + 1,
+                Variable = colnames(input.data[a]),
+                Category = "maxValue",
+                Message =  paste0(
+                  'Dataset variable contains entry value(s) greater than "maxValue" in dictionary'
+                )
+              )
+          }
 
-
-  for (a in 1:ncol(data.max)) {
-    for (bb in 1:nrow(dict.max))
-      if (colnames(data.max[a]) == dict.max$codeName[bb] &
-          max(unique(data.max[a]), na.rm = TRUE) > dict.max$maxValue[bb]) {
-        warnings.df <- warnings.df %>%
-          dplyr::add_row(
-            Num = nrow(warnings.df) + 1,
-            Variable = colnames(input.data[a]),
-            Category = "maxValue",
-            Message =  paste0(
-              'Dataset variable contains entry value(s) greater than "maxValue" in dictionary'
-            )
-          )
+        }
       }
-
   }
 
   assign("all.warnings", warnings.df)
